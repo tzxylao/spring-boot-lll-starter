f9a0551847cf4e5b7bc8ff615c84700b21ce826d
laoliangliang <15068610616@163.com>
2020-06-28 18:05:53 +0800

    fix

--- user/user-service-impl/src/main/java/com/company/project/userserviceimpl/UserApplication.java
+++ user/user-service-impl/src/main/java/com/company/project/userserviceimpl/UserApplication.java
@@ -1,16 +1,15 @@
 package com.company.project.userserviceimpl;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringCloudApplication
 @ComponentScan("com.company.project")
 @MapperScan(basePackages = {"com.company.project.*.mapper"})
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.company.project"})
 public class UserApplication {
 
